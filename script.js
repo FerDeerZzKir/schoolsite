cbb91708a8b50deb0e68d640f0cac7e5d7d128ee
schoolsite <121364308+FerDeerZzKir@users.noreply.github.com>
2024-02-05 11:57:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+document.getElementById("menu-button").addEventListener("click", function() {
+  var dropdownMenu = document.getElementById("dropdown-menu");
+  if (dropdownMenu.style.display === "block") {
+    dropdownMenu.style.display = "none";
+  } else {
+    dropdownMenu.style.display = "block";
+  }
+});
